667bae8d7ce3743ad00caf3d910cd63a685c71b9
alina <alina.rashid2002@gmail.com>
2024-02-28 23:59:07 +0400

    Added a reset and equal button. Added erase function which the reset button calls

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 //linking each button to display on text screen on click
 const buttons = document.querySelectorAll('button');
+const screen=document.querySelector('.displayScreenBar');
+
 //use forEach to iterate over NodeList
 buttons.forEach((button) => {
   button.addEventListener('click',displayButtonOnClick)
 });
 
+const reset=document.getElementById('reset');
+reset.addEventListener('click',erase);
+
+function erase(){
+    screen.innerText='';
+}
 
 function displayButtonOnClick(e){
-    const screen=document.querySelector('.displayScreenBar');
     screen.innerText+=e.target.innerText;
 }
