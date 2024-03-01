8f40aec194e6439347137561231f57dab449eda8
alina <alina.rashid2002@gmail.com>
2024-03-01 22:06:41 +0400

    added functions for operations and an operate function that is called when equal '=' button is clicked to do the calculation. Also added a function to display each button as it is clicked.

--- src/index.js
+++ src/index.js
@@ -1,33 +1,73 @@
-//linking each button to display on text screen on click
-const buttons = document.querySelectorAll('button');
-const screen=document.querySelector('.displayScreenBar');
+
+let firstNum='';
+let operator = "+";
+let secondNum = '';
+let change=false;
+let secondVariable='';
+
+
+const buttons = document.querySelectorAll("button");
+const screen = document.querySelector(".displayScreenBar");
 
 //use forEach to iterate over NodeList
 buttons.forEach((button) => {
-  button.addEventListener('click',displayButtonOnClick)
+  button.addEventListener("click", displayButtonOnClick);
 });
 
-function displayButtonOnClick(e){
-    if(e.target.id!='equal'){
-    screen.innerText+=e.target.innerText;
+function displayButtonOnClick(e) {
+  if(e.target.id=='equal'){
+    console.log(firstNum + ' '+operator+' '+ secondNum);
+    console.log(operate(firstNum,operator,secondNum));
+    screen.innerText=operate(firstNum,operator,secondNum);
+    firstNum=screen.innerText;
+    secondNum='';
+  }else if (!(e.target.id == "plus"||e.target.id == "minus"||e.target.id == "multiply"||e.target.id == "divide")) {
+    if(change){
+      secondNum+=e.target.innerText;
+      screen.innerText=secondNum;
     }
+    else{
+    firstNum += e.target.innerText;
+    screen.innerText=firstNum;
+    }
+
+  }else{
+    change=true;
+    operator=e.target.innerText;
+    screen.innerText=operator;
+  }
+
 }
 
-
-//function for erasing display screen text
-const reset=document.getElementById('reset');
-reset.addEventListener('click',erase);
-
-function erase(){
-    screen.innerText='';
+function operate(firstNum, operator, secondNum) {
+  firstNum=parseInt(firstNum);
+  secondNum=parseInt(secondNum);
+  if (operator == "+") {
+    return add(firstNum,secondNum);
+  }
+  else if(operator=='-'){
+    return subtract(firstNum, secondNum);
+  }
+  else if(operator=='x'){
+    return multiply(firstNum,secondNum);
+  }
+  else if(operator=='÷'){
+    return divide(firstNum,secondNum)
+  }
 }
 
-const equal=document.getElementById('equal');
-equal.addEventListener('click',add);
-//functions for operation
-function add(){
-    console.log(screen.innerText);
+function add(num1, num2) {
+  return num1 + num2;
 }
 
+function subtract(num1, num2) {
+  return num1 - num2;
+}
 
+function multiply(num1, num2) {
+  return num1*num2;
+}
 
+function divide(num1, num2) {
+  return num1 / num2;
+}
