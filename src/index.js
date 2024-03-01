4a0da2a285472f0d3a3e9be59446e0859abd7ff0
alina <alina.rashid2002@gmail.com>
2024-03-02 00:15:31 +0400

    formatted index.js to make it readable

--- src/index.js
+++ src/index.js
@@ -1,33 +1,27 @@
-
-let firstNum='';
+let firstNum = "";
 let operator = "+";
-let secondNum = '';
-let change=false;
-let secondVariable='';
-let sum='';
-
+let secondNum = "";
+let change = false;
 const screen = document.querySelector(".displayScreenBar");
 
-
 //function for erasing display screen text
-
 function erase() {
-  firstNum='';
+  firstNum = "";
   screen.innerText = "";
-  change=false;
+  change = false;
 }
 
 //function for deleting a digit
-function backspace(){
-let screenDisplay=screen.innerText;
-    let array=screenDisplay.split('');
-    array.splice(array.length-1,1);
-    console.log("in delete: "+array);
-    screen.innerText=array.join('');
-    firstNum=screen.innerText;
+function backspace() {
+  let screenDisplay = screen.innerText;
+  let array = screenDisplay.split("");
+  array.splice(array.length - 1, 1);
+  console.log("in delete: " + array);
+  screen.innerText = array.join("");
+  firstNum = screen.innerText;
 }
 
-//function for displaying ever button clicked
+//function for displaying every button clicked
 const buttons = document.querySelectorAll("button");
 
 //use forEach to iterate over NodeList
@@ -36,53 +30,50 @@ buttons.forEach((button) => {
 });
 
 function displayButtonOnClick(e) {
-  if(e.target.id=='equal'){
-    console.log(firstNum + ' '+operator+' '+ secondNum);
-    console.log(operate(firstNum,operator,secondNum));
-    screen.innerText=operate(firstNum,operator,secondNum);
-    firstNum=screen.innerText;
-    secondNum='';
-  }
-  else if(e.target.id=='reset'){
+  if (e.target.id == "equal") {
+    console.log(firstNum + " " + operator + " " + secondNum);
+    console.log(operate(firstNum, operator, secondNum));
+    screen.innerText = operate(firstNum, operator, secondNum);
+    firstNum = screen.innerText;
+    secondNum = "";
+  } else if (e.target.id == "reset") {
     erase();
-  }
-  else if(e.target.id=='delete'){
+  } else if (e.target.id == "delete") {
     backspace();
-
-  }
-  else if (!(e.target.id == "plus"||e.target.id == "minus"||e.target.id == "multiply"||e.target.id == "divide")) {
-    if(change){
-      secondNum+=e.target.innerText;
-      screen.innerText=secondNum;
+  } else if (
+    !(
+      e.target.id == "plus" ||
+      e.target.id == "minus" ||
+      e.target.id == "multiply" ||
+      e.target.id == "divide"
+    )
+  ) {
+    if (change) {
+      secondNum += e.target.innerText;
+      screen.innerText = secondNum;
+    } else {
+      console.log("screen text after C: " + screen.innerText);
+      firstNum += e.target.innerText;
+      screen.innerText = firstNum;
     }
-    else{
-    console.log("screen text after C: "+screen.innerText);
-    firstNum += e.target.innerText;
-    screen.innerText=firstNum;
-    }
-
-  }else{
-    change=true;
-    operator=e.target.innerText;
-    screen.innerText=operator;
+  } else {
+    change = true;
+    operator = e.target.innerText;
+    screen.innerText = operator;
   }
-
 }
 
 function operate(firstNum, operator, secondNum) {
-  firstNum=parseInt(firstNum);
-  secondNum=parseInt(secondNum);
+  firstNum = parseInt(firstNum);
+  secondNum = parseInt(secondNum);
   if (operator == "+") {
-    return add(firstNum,secondNum);
-  }
-  else if(operator=='-'){
+    return add(firstNum, secondNum);
+  } else if (operator == "-") {
     return subtract(firstNum, secondNum);
-  }
-  else if(operator=='x'){
-    return multiply(firstNum,secondNum);
-  }
-  else if(operator=='÷'){
-    return divide(firstNum,secondNum)
+  } else if (operator == "x") {
+    return multiply(firstNum, secondNum);
+  } else if (operator == "÷") {
+    return divide(firstNum, secondNum);
   }
 }
 
@@ -95,10 +86,9 @@ function subtract(num1, num2) {
 }
 
 function multiply(num1, num2) {
-  return num1*num2;
+  return num1 * num2;
 }
 
 function divide(num1, num2) {
   return num1 / num2;
 }
-
